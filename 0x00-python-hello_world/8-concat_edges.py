c1d6a69cd7d901368313435b0737e2a8e8b5730d
Greyshinobi2013 <getachewnatnael55@gmail.com>
2023-03-15 04:58:44 +0300

    speedy_mode

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str_1 = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str_1
+print(str_1[39:-64])
