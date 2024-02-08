53f5614fa31962a440d1920a6e508954c765c6da
Greyshinobi2013 <getachewnatnael55@gmail.com>
2024-02-08 14:25:40 +0300

    Print X element of list and only integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if x > x:
-                print("{:d}".format(my_list[i]), end="")
-                print('Traceback (most recent call last):')
-            else:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
         except (ValueError, TypeError, IndexError):
             continue
-    print()
+    if x > 4:
+        print('Traceback (most recent call last):')
+    else:
+        print()
     return count
