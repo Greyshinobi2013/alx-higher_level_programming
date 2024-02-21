a78a9055b96c0dff443dc62c9c31975ffd729fe8
Greyshinobi2013 <getachewnatnael55@gmail.com>
2024-02-21 22:36:17 +0300

    Class that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,20 @@
 """
-Python program that inherits from list
+Return list of available attributes and  methods of an object.
 """
 
 
 class MyList(list):
     """
-    My list inherits from list
+    My list class inherits list object.
     """
+
     def print_sorted(self):
         """
         Prints the list in ascending order.
+        Ar:gs:
+            self: of object
+        Return:
+            list of object
         """
         sorted_list = sorted(self)
-        print(sorted_list)
+        return sorted_list
