9e9cb9b179befb0727305ca93662d6bdc54a485d
Greyshinobi2013 <getachewnatnael55@gmail.com>
2024-04-28 20:50:19 +0300

    Validate size attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,6 +4,7 @@ class LockedClass:
     """
     This class is locked class.
     Attribute:
-            __slots__ : used to declare a fixed set of attribute that instances of the class can have. In this case 'first_name'
+            __slots__ : used to declare a fixed set of attribute that instances of the class can have.
+            In this case 'first_name'
     """
     __slots__ = ['first_name']
