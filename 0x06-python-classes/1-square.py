263239d5282d4b4da60e596e9f30737e23751524
Greyshinobi2013 <getachewnatnael55@gmail.com>
2024-04-28 15:32:53 +0300

    Create private attribute in side 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Square class
+    instance of private attribute known as size
+
+"""
+
+
+class Square:
+    """
+    Attribute:
+        size(int): private attribute
+                syntax: __size
+    """
+
+    def __init__(self, size):
+        """
+        Attribute:
+            __size:(int)
+        """
+        self.__size = size
